f70dbd14ae901db5c253f7b09ad99f62b6d49ad9
Gagneyahmeri <pyry.anttila@gmail.com>
2026-02-17 04:43:05 -0500

    configs and other scripts

---
+++ challenge/deploy.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+set -e
+export PATH=$PWD/../bin:$PWD/bin:$PATH
+
+echo "[*] Deploying CTF Challenge..."
+
+# 1. Apply Manifests
+kubectl apply -f manifests/target.yaml
+kubectl apply -f manifests/rbac.yaml
+
+# 2. Wait for Target
+echo "[*] Waiting for victim-db..."
+kubectl wait --for=condition=ready pod/victim-db --timeout=60s
+
+# 3. Generate Player Config
+echo "[*] Generating 'player.kubeconfig'..."
+# Create a long-lived token for the service account
+TOKEN=$(kubectl create token monitor-dev --duration=24h)
+SERVER=$(kubectl config view --minify -o jsonpath='{.clusters[0].cluster.server}')
+CA_DATA=$(kubectl config view --minify --raw -o jsonpath='{.clusters[0].cluster.certificate-authority-data}')
+
+# Write the config file
+cat <<EOF > ../player.kubeconfig
+apiVersion: v1
+kind: Config
+clusters:
+- cluster:
+    certificate-authority-data: $CA_DATA
+    server: $SERVER
+  name: ctf-cluster
+users:
+- name: monitor-dev
+  user:
+    token: $TOKEN
+contexts:
+- context:
+    cluster: ctf-cluster
+    user: monitor-dev
+  name: ctf-context
+current-context: ctf-context
+EOF
+
+echo "--------------------------------------------------"
+echo "CHALLENGE DEPLOYED!"
+echo "The file 'player.kubeconfig' has been created in the root folder."
+echo "Hand this file to the player."
+echo "--------------------------------------------------"
